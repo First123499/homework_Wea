11f4b9311ce0bbae7da4a6d127304f4bc692dbc6
First123499 <32410701+First123499@users.noreply.github.com>
2017-10-02 03:54:42 +0700

    Add files via upload

--- weather.js
+++ weather.js
@@ -9,27 +9,27 @@ $(function () {
         $.get(urlApi, function (data) {
             var weatherC = data.weather[0].main;
             if (weatherC == "Clouds") {
-                var weatherS = "Cloudy.png";
+                var weatherS = "2.png";
             }
             if (weatherC == "Rain") {
-                var weatherS = "Rain.png";
+                var weatherS = "6.png";
             }
             if (weatherC == "Clear") {
-                var weatherS = "clear.png";
+                var weatherS = "3.png";
             }
             if (weatherC == "Thunderstorm") {
-                var weatherS = "Thunderstorm.png";
+                var weatherS = "5.png";
             }
             if (weatherC == "Mist") {
-                var weatherS = "mist.png";
+                var weatherS = "14.png";
             }
             console.log(data);
-            var row1 = "<h4 class = 'fontH'><img id = 'pic' src='location.png' alt=''><b>" + city + "</td><td><h4><img id = 'pic' src='date.png' alt=''><b>" + date + "</h4></td></tr></table>" +
+            var row1 = "<h4 class = 'fontH'><img id = 'pic' src='' alt=''><b>" + city + "</td><td><h4><img id = 'pic' src='' alt=''><b>" + date + "</h4></td></tr></table>" +
                 "<table align = 'center'><tr><td><img class = 'font' src='" + weatherS +
                 "' alt=''></td></tr></table><table align = 'center'><tr><td width = '100%'><p id = 'text'><b>" + data.weather[0].description +
-                "<b></p></td></tr></table>" + "<table>" + "<tr><td><img src='Celsius.png' alt=''></td><td>" + ((data.main.temp) / 10).toFixed(2) +
-                " °C</td></tr>" + "<tr><td><img src='Pressure.png' alt=''></td><td>" + data.main.pressure +
-                " hPa</td></tr>" + "<tr><td><img src='humidity.png' alt=''></td><td>" + data.main.humidity + "%";
+                "<b></p></td></tr></table>" + "<table>" + "<tr><td><img src='' alt=''></td><td>" + ((data.main.temp) / 10).toFixed(2) +
+                " °C</td></tr>" + "<tr><td><img src='' alt=''></td><td>" + data.main.pressure +
+                " hPa</td></tr>" + "<tr><td><img src='' alt=''></td><td>" + data.main.humidity + "%";
             $("#forecastbox").append(row1);
         });
     });
